2e4d5984865e77b1fa6c193efddf5bbc90018d69
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2023-01-17 14:55:10 -0500

    Created docker compose file with required services

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
-
 const app = express();
 
 const { PORT } = process.env;
 console.log(PORT)
+
+app.post('/testpostreq', (req,res) => {
+    res.status(200).send("Hello There From API");
+})
+
+app.post('/testgetreq', (req,res) => {
+    res.status(200).send("Hello There From API");
+})
 
 app.listen(PORT, () => console.log(`API is listening on http://localhost:${PORT}`));
