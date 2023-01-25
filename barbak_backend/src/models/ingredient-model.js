88a82d22831282e7ebf63a7ac39cdcf7d0cd8780
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2023-01-25 01:41:59 -0500

    Made modifications to Ingredient Model

--- barbak_backend/src/models/ingredient-model.js
+++ barbak_backend/src/models/ingredient-model.js
@@ -1,25 +1,120 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 // Base Ingredient Schema
+// const ingredientSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         minLength: 3,
+//         maxLength: 30,
+//         lowercase: true,
+//         required: true,
+//     },
+//     description: {
+//         type: String,
+//         maxLength: 500,
+//     },
+//     category: {
+//         type: String,
+//         required: true,
+//         lowercase: true,
+//         enum: {
+//             values: ['alcohol', 'beverage', 'juice', 'fruit', 'other'],
+//             message: props => `${props.value} is not a valid 'category' state`
+//         }
+//     },
+//     user: {
+//         type: mongoose.SchemaTypes.ObjectId,
+//         required: true,
+//         immutable: true,
+//     },
+//     visibility: {
+//         type: String,
+//         required: true,
+//         lowercase: true,
+//         enum: {
+//             values: ['private', 'public', 'in-review'],
+//             message: props => `${props.value} is not a valid 'visibility' state`,
+//         }
+//     },
+//     creation_date: {
+//         type: Date,
+//         immutable: true,
+//         default: () => Date.now(),
+//     }
+// }, { collection: 'ingredients' });
+
+
+// // Derived Alcohol Schema
+// const alcoholicIngredientSchema = new mongoose.Schema({
+//     alcohol_category: {
+//         type: String,
+//         lowercase: true,
+//         required: true,
+//         enum: {
+//             values: ['beer', 'wine', 'liquor', 'liqueur', 'other'],
+//             message: props => `${props.value} is not a valid 'alcohol_category' state`,
+//         }
+//     },
+//     alcohol_by_volume: {
+//         type: Array,
+//         validate: {
+//             validator: val => {
+//                 if(val.length > 2)
+//                     return false;
+//                 var prev = -1;
+//                 for(var i = 0; i < val.length; i++) {
+//                     if(isNaN(val[i]) || val[i] > 100 || val[i] < 0 || prev > val[i])
+//                         return false;
+//                     prev = val[i];
+//                 }
+//                 return true;
+//             },
+//             message: 'provided value is invalid',
+//         }
+//     }
+// }, { collection: 'ingredients' });
+
+// const Ingredient = mongoose.model("ingredient", ingredientSchema);
+// const AlcoholicIngredient = Ingredient.discriminator('alcoholic-ingredient', alcoholicIngredientSchema);
+
+// module.exports = { Ingredient, AlcoholicIngredient }
+
+const typeCategories = {
+    liquor: [ "whiskey", "gin", "vodka", "rum", "tequila", "brandy" ],
+    liqueur: [ "orange", "coffee", "cream", "nut", "herb", "fruit" ],
+    beer: [ "lager", "ale", "wheat", "stout", "porter", "sour", "belgia" ],
+    wine: [ "red", "white", "rose", "sparkling", "fortified" ],
+    mixer: [ "juice", "syrup", "soda", "dairy", "bitter", "spice", "herbs" ],
+    fruit: [ "citrus", "berries", "melons", "tropical", "stone", "pome" ],
+    other: [ "alcoholic", "non-alcoholic" ]
+};
+
 const ingredientSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
         maxLength: 30,
-        lowercase: true,
         required: true,
     },
     description: {
         type: String,
-        maxLength: 500,
+        maxLength: 500
+    },
+    type: {
+        type: String,
+        required: true,
+        validate: {
+            validator: val => _.includes(Object.keys(typeCategories), val)
+        }
     },
     category: {
         type: String,
         required: true,
-        lowercase: true,
-        enum: {
-            values: ['alcohol', 'beverage', 'juice', 'fruit', 'other'],
-            message: props => `${props.value} is not a valid 'category' state`
+        validate: {
+            validator: function(val) {
+                return _.includes(typeCategories[this.type], val);
+            }
         }
     },
     user: {
@@ -43,18 +138,7 @@ const ingredientSchema = new mongoose.Schema({
     }
 }, { collection: 'ingredients' });
 
-
-// Derived Alcohol Schema
 const alcoholicIngredientSchema = new mongoose.Schema({
-    alcohol_category: {
-        type: String,
-        lowercase: true,
-        required: true,
-        enum: {
-            values: ['beer', 'wine', 'liquor', 'liqueur', 'other'],
-            message: props => `${props.value} is not a valid 'alcohol_category' state`,
-        }
-    },
     alcohol_by_volume: {
         type: Array,
         validate: {
@@ -69,7 +153,7 @@ const alcoholicIngredientSchema = new mongoose.Schema({
                 }
                 return true;
             },
-            message: 'provided value is invalid',
+            message: "provided value for 'alcohol_by_volume' is invalid",
         }
     }
 }, { collection: 'ingredients' });
--- barbak_backend/src/models/tool-model.js
+++ barbak_backend/src/models/tool-model.js
@@ -1,15 +1,63 @@
 const mongoose = require('mongoose');
 
+// const toolSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         minLength: 3,
+//         maxlength: 30,
+//         required: true,
+//     },
+//     description: {
+//         type: String,
+//         maxLength: 500,
+//     },
+//     user: {
+//         type: mongoose.SchemaTypes.ObjectId,
+//         required: true,
+//         immutable: true,
+//     },
+//     visibility: {
+//         type: String,
+//         required: true,
+//         lowercase: true,
+//         enum: {
+//             values: ['private', 'public', 'in-review'],
+//             message: props => `${props.value} is not a valid 'visibility' state`,
+//         }
+//     },
+//     creation_date: {
+//         type: Date,
+//         required: true,
+//         immutable: true,
+//         default: () => Date.now(),
+//     }
+// }, { collection: 'tools' });
+
+const toolTypes = [ "mixing", "measuring", "stirring", "muddling", "straining", "opening", "serving", "pouring", "garnishing", "cutting", "chilling", "cleaning", "other" ];
+
+const toolMaterials = [ "stainless-steel", "brass", "wood", "plastic", "aluminum", "silicone", "glass", "ceramic", "titanium", "graphite", "other" ];
+
+const toolVisibilities = ['private', 'public', 'in-review'];
+
 const toolSchema = new mongoose.Schema({
     name: {
         type: String,
         minLength: 3,
-        maxlength: 30,
-        required: true,
+        maxLength: 30,
+        required: true
     },
     description: {
         type: String,
-        maxLength: 500,
+        maxLength: 500
+    },
+    type: {
+        type: String,
+        enum: toolTypes,
+        required: true
+    },
+    material: {
+        type: String,
+        enum: toolMaterials,
     },
     user: {
         type: mongoose.SchemaTypes.ObjectId,
@@ -21,7 +69,7 @@ const toolSchema = new mongoose.Schema({
         required: true,
         lowercase: true,
         enum: {
-            values: ['private', 'public', 'in-review'],
+            values: toolVisibilities,
             message: props => `${props.value} is not a valid 'visibility' state`,
         }
     },
