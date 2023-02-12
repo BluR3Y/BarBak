49fe9e3d00b77dbafa1c1a88a7bf731b016037a3
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2023-02-11 23:48:59 -0500

    Incorporated React Redux

--- barbak_client/src/pages/_app.js
+++ barbak_client/src/pages/_app.js
@@ -3,6 +3,11 @@ import { useEffect, useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { siteThemes } from "@/styles/utils/themes";
 
+// React Redux Imports
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "@/redux/store";
+import { Provider } from "react-redux";
+
 export default function App({ Component, pageProps }) {
   const [activeTheme, setActiveTheme] = useState('classic');
 
@@ -22,8 +27,12 @@ export default function App({ Component, pageProps }) {
     setActiveTheme(event.matches ? 'dark' : 'classic');
   }
 
-  return <ThemeProvider theme={siteThemes[activeTheme]}>
-    <GlobalStyles/>
-    <Component {...pageProps} />
-  </ThemeProvider>;
+  return <Provider store={store}>
+    <PersistGate loading={<h1>Loading...</h1>} persistor={persistor}>
+      <ThemeProvider theme={siteThemes[activeTheme]}>
+        <GlobalStyles/>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
 }
---
+++ barbak_client/src/redux/actions.js
@@ -0,0 +1,23 @@
+export const SET_USER_ID = 'SET_USER_ID';
+export const SET_USER_NAME = 'SET_USER_NAME';
+export const SET_USER_EMAIL = 'SET_USER_EMAIL';
+export const SET_USER_PROFILE_IMAGE = 'SET_USER_PROFILE_IMAGE';
+
+export const setUserInfo = userInfo => dispatch => {
+    dispatch({
+        type: SET_USER_ID,
+        payload: userInfo.user_id,
+    });
+    dispatch({
+        type: SET_USER_NAME,
+        payload: userInfo.user_name,
+    });
+    dispatch({
+        type: SET_USER_EMAIL,
+        payload: userInfo.user_email,
+    });
+    dispatch({
+        type: SET_USER_PROFILE_IMAGE,
+        payload: userInfo.user_profile_image
+    });
+};
