236a84eaa026c00df292ed5216666710af374b7b
BluR3Y <69604218+BluR3Y@users.noreply.github.com>
2023-05-18 22:59:20 -0400

    modified mysql config file

--- database/mysql/init/config.sh
+++ database/mysql/init/config.sh
@@ -1,2 +1,6 @@
-mysql -u root -p$MYSQL_ROOT_PASSWORD --execute "ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD'";
+#!/bin/bash
+mysql -u root -p$MYSQL_ROOT_PASSWORD -h localhost --execute \
+"
+ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
+"
